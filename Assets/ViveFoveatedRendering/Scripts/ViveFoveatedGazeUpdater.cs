c39adbb35d4d84cc8c6b99dcc627404ff025628c
cyy0915 <33276595+cyy0915@users.noreply.github.com>
2019-11-30 23:58:24 +0800

    Add files via upload

---
+++ Assets/ViveFoveatedRendering/Scripts/ViveFoveatedGazeUpdater.cs
@@ -0,0 +1,40 @@
+﻿//========= Copyright 2019, HTC Corporation. All rights reserved. ===========
+
+using UnityEngine;
+
+
+namespace HTC.UnityPlugin.FoveatedRendering
+{
+    public class ViveFoveatedGazeUpdater : MonoBehaviour
+    {
+        public static bool AttachGazeUpdater(GameObject obj)
+        {
+            if (obj != null)
+            {
+                var gazeUpdater = obj.GetComponent<ViveFoveatedGazeUpdater>();
+                if(gazeUpdater == null)
+                {
+                    gazeUpdater = obj.AddComponent<ViveFoveatedGazeUpdater>();
+                }
+
+                gazeUpdater.enabled = true;
+
+                return true;
+            }
+            return false;
+        }
+        
+        void OnEnable()
+        {
+
+        }
+        
+        void Update()
+        {
+            //这里输入每帧的左右眼预测位置，位置为normolized的向量，如(0.0f, 0.0f, 1.0f)就是看向z轴正方向，坐标系为观察坐标系
+            ViveFoveatedRenderingAPI.SetNormalizedGazeDirection(new Vector3(0.0f, 0.0f, 1.0f), new Vector3(0.0f, 0.0f, 1.0f));
+            GL.IssuePluginEvent(ViveFoveatedRenderingAPI.GetRenderEventFunc(), (int)EventID.UPDATE_GAZE);
+        }
+
+    }
+}
